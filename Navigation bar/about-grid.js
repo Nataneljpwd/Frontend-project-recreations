3b7c876947e054d7f7335ce35971237d8d55f4f1
Natanel Rudyuklakir <natanelrudyuklakir@gmail.com>
2022-11-17 12:25:55 +0200

    continuation of the about page

--- Navigation bar/about-grid.js
+++ Navigation bar/about-grid.js
@@ -3,7 +3,7 @@ const cellSize=30;
 const width=window.innerWidth;
 const height=window.innerHeight;
 
-const grid=document.getElementById("main-grid");
+const grid=document.querySelector(".grid-container");
 let numRows=Math.floor(height/cellSize);
 let numCols=Math.floor(width/cellSize);
 grid.innerHTML="";
@@ -13,7 +13,24 @@ for(let i=0;i<numRows*numCols;i++){
     cell.classList.add("cell");
     cells.push(cell);
 }
-cells.forEach((cell)=>grid.appendChild(cell));
+let matrix=[];
+for(let i = 0; i < cells.length; i += numCols) {
+    matrix.push(cells.slice(i, i + numCols));
+}
+matrix.forEach((row)=>{
+    row.forEach((col)=>{
+        grid.appendChild(col);
+        col.addEventListener("click",(e)=>{
+            let rawX=e.clientX;
+            let rawY=e.clientY;
+
+            let currRow=Math.floor((rawY*cellSize)/height);
+            let currCol=Math.floor((rawX*cellSize)/width);
+
+            expand(currRow,currCol,matrix);
+        });
+    })
+});
 grid.style.setProperty("--rows",numRows);
 grid.style.setProperty("--cols",numCols);
 
@@ -21,13 +38,72 @@ window.addEventListener("resize",(e)=>{
     let numRows=Math.floor(window.innerHeight/cellSize);
     let numCols=Math.floor(window.innerWidth/cellSize);
     grid.innerHTML="";
-    let cells=[];
-    for(let i=0;i<numRows*numCols;i++){
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        cells.push(cell);
+    const matrix=[];
+    for(let i=0;i<numRows;i++){
+        matrix.push(new Array(numCols));
+        for(let j=0;j<numCols;j++){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            matrix[i].push(cell);
+        }
     }
-    cells.forEach((cell)=>grid.appendChild(cell));
+    
+    matrix.forEach((row)=>{
+        row.forEach((col)=>{
+            grid.appendChild(col);
+            col.addEventListener("click",(e)=>{
+                let rawX=e.clientX;
+                let rawY=e.clientY;
+                const set=new Set();
+
+                let currRow=Math.floor(rawY/cellSize);
+                let currCol=Math.floor(rawX/cellSize);
+
+                expand(currRow,currCol,matrix,set);
+            });
+        })
+    });
     grid.style.setProperty("--rows",numRows);
     grid.style.setProperty("--cols",numCols);
+
 });
+
+const expand = (row,coll,matrix,set)=>{
+    set.add(row+" "+coll);
+    let c=matrix[row][coll];
+    console.log(c.classList);
+    console.log(matrix[row][coll]);
+    c.classList.toggle("hideCell");
+    if(row+1<matrix.length && !set.has((row+1)+" "+coll)){
+        set.add((row+1)+" "+coll);
+        expand(row+1,coll,matrix,set);
+        if(coll+1<matrix[row].length && !set.has((row+1)+" "+(coll+1))){
+            set.add((row+1)+" "+(coll+1));
+            expand(row+1,coll+1,matrix,set);
+            if(!set.has(row+" "+(coll+1))){
+                set.add(row+" "+(coll+1));
+                expand(row,coll+1,matrix,set);
+            }
+        }
+        if(coll-1>=0 && !set.has((row+1)+" "+(coll-1))){
+            set.add((row+1)+" "+(coll-1));
+            expand(row+1,coll-1,matrix,set);
+            if(!set.has(row+" "+(coll-1))){
+                set.add(row+" "+(coll-1));
+                expand(row,coll-1,matrix,set);
+            }   
+        }
+    }
+    if(row-1>=0){
+        set.add((row-1)+" "+coll);
+        expand(row-1,coll,matrix,set);
+        if(coll+1<matrix[row].length && !set.has((row-1)+" "+(coll+1))){
+            set.add((row-1)+" "+(coll+1));
+            expand(row-1,coll+1,matrix,set);
+        }
+        if(coll-1>=0 && !set.has((row-1)+" "+(coll-1))){
+            set.add((row-1)+" "+(coll-1));
+            expand(row-1,coll-1,matrix);
+        }
+    }
+}
