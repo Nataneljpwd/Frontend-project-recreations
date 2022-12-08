2dad9678fbc910934142cae46f39b002ad7c85a5
Natanel Rudyuklakir <natanelrudyuklakir@gmail.com>
2022-12-08 12:16:55 +0200

    end of basic pawn tutor, will polish after others.

--- Navigation bar/about-grid.js
+++ Navigation bar/about-grid.js
@@ -24,40 +24,13 @@ grid.style.setProperty("--rows", numRows);
 grid.style.setProperty("--cols", numCols);
 
 
-
 window.addEventListener("resize", (e) => {
     numRows = Math.floor(window.innerHeight / cellSize);
     numCols = Math.floor(window.innerWidth / cellSize);
     grid.innerHTML = "";
-    //for (let i = 0; i < numCols*numRows; i++) {
-        //for (let j = 0; j < numCols; j++) {
-            //let cell = document.createElement("div");
-            //cell.classList.add("cell");
-            //cell.dataset.rowNum = i;
-            //cell.dataset.columnNum = j;
-            //cell.style.opacity=toggled? "0":"1";
-            //grid.appendChild(cell);
-            //cell.addEventListener("click",expand(i,numRows,numCols));
-        //}
-    //}
   Array.from(Array(numCols*numRows)).map((cell, index) => {
     grid.appendChild(createTile(index));
   });
-    // matrix.forEach((row) => {
-    //     row.forEach((col) => {
-    //         grid.appendChild(col);
-    //         col.addEventListener("click", (e) => {
-    //             let rawX = e.clientX;
-    //             let rawY = e.clientY;
-
-    //             let currRow = Math.floor(rawY / cellSize);
-    //             let currCol = Math.floor(rawX / cellSize);
-    //             console.log(currRow, currCol);
-    //             toggled=!toggled;
-    //             expand(currRow, currCol,numRows,numCols);
-    //         });
-    //     })
-    // });
     grid.style.setProperty("--rows", numRows);
     grid.style.setProperty("--cols", numCols);
 
@@ -74,24 +47,4 @@ const expand = (index,numRows,numCols) => {
       from: index
     })
   });
-    /*let set = new Set();
-    let q = [];
-    //use push and shift so itll work like a q
-    q.push(document.querySelector('.cell[data-row-num = "' + row + '"][data-column-num = "' + col + '"]'));
-    set.add(row + " " + col);
-    let dirs = [[1, 0], [1, 1], [1, -1], [0, 1], [0, -1], [-1, 1], [-1, 0], [-1, -1]];
-    let k=0;
-    while (q.length > 0) {
-        let curr = q.shift();
-        curr.style.opacity = curr.style.opacity == "1" ? "0" : "1";
-        k++;
-        for (let i = 0; i < dirs.length; i++) {
-            if (parseInt(curr.dataset.rowNum) + dirs[i][0] < numRows && parseInt(curr.dataset.rowNum) + dirs[i][0] >= 0 && parseInt(curr.dataset.columnNum) + dirs[i][1] < numCols && parseInt(curr.dataset.columnNum) + dirs[i][1] >= 0 && !set.has((parseInt(curr.dataset.rowNum) + dirs[i][0]) + " " + (parseInt(curr.dataset.columnNum) + dirs[i][1])) ) {
-                set.add((parseInt(curr.dataset.rowNum) + dirs[i][0]) + " " + (parseInt(curr.dataset.columnNum) + dirs[i][1]));
-                let cell=document.querySelector('.cell[data-row-num = "' + (parseInt(curr.dataset.rowNum) + dirs[i][0]) + '"][data-column-num ="' + (parseInt(curr.dataset.columnNum) + dirs[i][1]) + '"]');
-                cell.style.transitionDelay= k+"ms";
-                q.push(cell);
-            }
-        }
-    }*/
 }
--- Navigation bar/howToPlay.js
+++ Navigation bar/howToPlay.js
@@ -1,41 +1,66 @@
 const boards = document.querySelectorAll('.board');
-const [rightPawn,leftPawn] = document.querySelectorAll('.pawn-piece');
+const [whitePawn,blackPawn] = document.querySelectorAll('.pawn-piece');
 const next = document.querySelector(".next");
 const previous = document.querySelector(".prev");
 const pieceTutor = [];
 const cellSize = 4.375;//to put the thing in cell center in vmax
-const screenNum=0;
-const currPlaceInTutor=0;
+let screenNum=0;
+let currPlaceInTutor=0;
 let firstClick=true;
+let correctMove=false;
 const textArea=document.getElementById("text");
 //make a move func to replace so that we can movew things back
 //make the tutor so that we press where to move and if it is valid , we continue.
-    const pawnTutor = {
-        //first we show how the piece moves:
-        //the first move:
-        firstMove: {initialPosWhitePawn:[2,1], initialPosBlackPawn:[] ,correctMove:[3,1], initialText:"The Pawn can be moved 1 sqaure forwards if there is no piece blocking it, or if there wont be a check after the move. \n Try it youself, click on the piece and then the square to move is to", correctText:"Correct, click next to move on.", incorrectText:"wrong move, please try again."},
-        //show the secont possible move so first go back:
-    };
-    
+const pawnTutor = [
+    {initialPosWhitePawn:[2,1], initialPosBlackPawn:[] ,correctMove:[3,1], initialText:"The Pawn can be moved 1 sqaure forwards if there is no piece blocking it, or if there wont be a check after the move. Try it youself, click on the piece and then the square to move to", correctText:"Correct, click next to move on.", incorrectText:"wrong move, please try again."},
+    {initialPosWhitePawn:[2,1], initialPosBlackPawn:[], correctMove:[4,1],initialText:"if it is the first move and the previous rules apply, we can advance the pawn 2 squares forwards, try it yourself!.",correctText:"Correct, click next to move on.", incorrectText:"Wrong move, please try again."},
+    {initialPosWhitePawn:[4,3], initialPosBlackPawn:[5,4], correctMove:[5,4], initialText:"If there is a piece to the right and in front of the pawn, we can take it, try it yourself!.", correctText:"Correct, click next to move on.", incorrectText:"Wrong move, please try again"},
+    {initialPosWhitePawn:[4,3], initialPosBlackPawn:[5,2], correctMove:[5,2], initialText:"If there is a piece to the left and in front of the pawn, we can take it, try it yourself!.", correctText:"Correct, click next to move on.", incorrectText:"Wrong move, please try again"}
+];
+pieceTutor.push(pawnTutor);
+let currTutor=pieceTutor[screenNum];
 
 next.onclick = () => {
-    currPlaceInTutor++;
-    if(currPlaceInTutor >= pawnTutor[screenNum].keys().length) {
-        screenNum++;
-        currPlaceInTutor = 0;
+    if(correctMove){
+        currPlaceInTutor++;
+        if(currPlaceInTutor >= pieceTutor[screenNum].length) {
+            screenNum++;
+            currPlaceInTutor = 0;
+        }
+        screenNum%=pieceTutor.length;
+        currTutor=pieceTutor[screenNum];
+        pawnTutorSetup();
     }
 }
 
+const pawnTutorSetup = () =>{
+    textArea.innerHTML = currTutor[currPlaceInTutor].initialText;
+    let anim={
+        bottom: parseInt((currTutor[currPlaceInTutor].initialPosWhitePawn[0])-1)*cellSize +0.5 + 'vmax',
+        left: parseInt((currTutor[currPlaceInTutor].initialPosWhitePawn[1]))*cellSize +0.5 + 'vmax'
+    }
+    whitePawn.animate(anim,{ duration: 400, fill: "forwards"});
+    if(currTutor[currPlaceInTutor].initialPosBlackPawn.length !== 0){
+        blackPawn.classList.remove("hide");
+        anim={
+            bottom: parseInt((currTutor[currPlaceInTutor].initialPosBlackPawn[0])-1)*cellSize +0.5 + 'vmax',
+              left: parseInt((currTutor[currPlaceInTutor].initialPosBlackPawn[1]))*cellSize +0.5 + 'vmax'
+        }
+        whitePawn.style.zIndex=12;
+        blackPawn.animate(anim,{ duration: 400, fill: "forwards"});
+        
+    }
+    correctMove=false;
+}
 
-const currTutor=pawnTutor.firstMove;
+pawnTutorSetup();
 
 const handleClick = (e) => {
     let col=e.target.dataset.col;
     let row=e.target.dataset.row;
-    console.log(row,col);
-    if(col == currTutor.initialPosWhitePawn[1] && row == currTutor.initialPosWhitePawn[0]){
+    if(col == currTutor[currPlaceInTutor].initialPosWhitePawn[1] && row == currTutor[currPlaceInTutor].initialPosWhitePawn[0]){
         firstClick=false;
-        console.log("click");
+        console.log("inside--------");
     }
     else if(!firstClick){
         let lef = parseInt(col) * cellSize + 0.5;
@@ -44,19 +69,20 @@ const handleClick = (e) => {
             bottom: botto + 'vmax',
             left: lef + 'vmax'
         }
-        rightPawn.animate(anim, { duration: 400, fill: "forwards" });
-        if(row == currTutor.correctMove[0] && col == currTutor.correctMove[1]){
+        whitePawn.animate(anim, { duration: 400, fill: "forwards" });
+        if(row == currTutor[currPlaceInTutor].correctMove[0] && col == currTutor[currPlaceInTutor].correctMove[1]){
             //set the text to correctText
-            textArea.innerHTML=currTutor.correctText;
+            textArea.innerHTML=currTutor[currPlaceInTutor].correctText;
+            correctMove=true;
         }else{
-            textArea.innerHTML=currTutor.incorrectText;
+            textArea.innerHTML=currTutor[currPlaceInTutor].incorrectText;
             //move back
             setTimeout(()=>{
                 anim={
-                bottom: parseInt((currTutor.initialPosWhitePawn[0])-1)*cellSize +0.5 + 'vmax',
-                left: parseInt((currTutor.initialPosWhitePawn[1]))*cellSize +0.5 + 'vmax'
-            }
-            rightPawn.animate(anim,{duration:400, fill:"forwards"});
+                bottom: parseInt((currTutor[currPlaceInTutor].initialPosWhitePawn[0])-1)*cellSize +0.5 + 'vmax',
+                left: parseInt((currTutor[currPlaceInTutor].initialPosWhitePawn[1]))*cellSize +0.5 + 'vmax'
+                }
+            whitePawn.animate(anim,{duration:400, fill:"forwards"});
             },500);
         }
         firstClick=!firstClick;
